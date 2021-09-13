71a1061fbac30b04403943d61e896a0d26cf8745
pseud0n <pseud0n@users.noreply.github.com>
2021-09-13 21:05:15 +0000

    Modified for pi/laptop share

---
+++ laptop.nix
@@ -0,0 +1,18 @@
+{config, lib, pkgs, ...}:
+{
+	boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+	boot.kernelModules = [ "wl" ]; # set of kernel modules loaded in second stage of boot process
+	boot.initrd.kernelModules = [ "kvm-intel" "wl" ]; # list of modules always loaded by the initrd (initial ramdisk)
+	hardware.opengl.driSupport32Bit = true;
+
+	# Enable CUPS to print documents.
+	# https://nixos.wiki/wiki/Printing
+	services.printing = if isPi then {} else {
+		enable = true;
+		drivers = with pkgs; [
+			# Pixma
+			cnijfilter2
+		];
+	};
+}
+
---
+++ pi.nix
@@ -0,0 +1,5 @@
+{config, lib, pkgs, ...}:
+{
+	hardware.raspberry-pi."4".fkms-3d.enable = true;
+}
+
