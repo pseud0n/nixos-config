4465837e134bc8ec482abe31abc972fe99120137
pseud0n <pseud0n@users.noreply.github.com>
2021-09-19 21:14:53 +0100

    Modified for more customisation

--- home-manager/config/sway/scripts/open-terminal-cd.bash
+++ home-manager/config/sway/scripts/open-terminal-cd.bash
@@ -1,13 +1,14 @@
 #! /usr/bin/env bash
+command=$1
 pid=$(swaymsg -t get_tree | jq '.. | select(.type?) | select(.focused==true).pid')
 
-echo "$(ps -ef | grep fish)" > ~/log
+#echo "$(ps -ef | grep fish)" > ~/log
 
 process_match=$(ps -ef | grep fish | awk '$3 == pid { print $2 }' pid=$pid)
 
 if [ -z $process_match ]
 then
-	echo "alacritty"
+	echo "$command ~"
 else
-	echo "alacritty -e fish -C $(pwdx $process_match | awk '{ print $2 }')"
+	echo "$command $(pwdx $process_match | awk '{ print $2 }')"
 fi
